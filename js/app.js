88315fae507acfa2947013a70d579f1b5ee08a07
mustafa-mahmud <mithuweb000@gmail.com>
2021-08-30 17:43:38 +0600

    Select end

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ const inputName = document.querySelector('input[name="name"]');
 const inputAge = document.querySelector('input[name="age"]');
 const inputPassword = document.querySelector('input[name="password"]');
 const inputFile = document.querySelector('input[name="img-file"]');
+const tbodyEl = document.querySelector('tbody');
 
 function formValidation(e) {
   e.preventDefault();
@@ -48,10 +49,45 @@ async function insertData(datas) {
   if (data.includes('successfully')) {
     clearAll(inputName, inputAge, inputPassword, inputFile);
     message('green', 'Inserted Successfully');
+    read();
   } else {
     message('red', 'Something went wrong, Please try again...');
   }
 }
 
+async function read() {
+  const res = await fetch('./php/read.php');
+  const data = await res.json();
+
+  console.log(data);
+  if (data) {
+    displayUI(data);
+  }
+}
+
+function displayUI(datas) {
+  tbodyEl.innerHTML = '';
+  datas.forEach((el, ind) => {
+    const html = `
+		<tr id="${el.id}">
+			<td>${ind + 1}</td>
+			<td>${el.name}</td>
+			<td>${el.age}</td>
+			<td>
+				<img width="50" height="30" src="uploads/${el.img}" alt="${el.name}" />
+			</td>
+			<td>
+				<span class="change-password">Change Password</span> |
+				<span class="edit">Edit</span> |
+				<span class="delete">Delete</span>
+			</td>
+		</tr>
+		`;
+
+    tbodyEl.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
 /////////////////
+read();
 form.addEventListener('submit', formValidation);
---
+++ php/read.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'db.php';
+
+if (isset($_GET)) {
+  $sql = "SELECT * FROM users1";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+
+  if ($stmt->rowCount()) {
+    echo json_encode($stmt->fetchAll());
+  }
+}
+?>
